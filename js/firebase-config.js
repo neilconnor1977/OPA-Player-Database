964e363e7083cfbd810f6820e840a37708c120ec
neilconnor1977 <neilconnor1977@gmail.com>
2025-08-27 18:21:56 +0100

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  "apiKey": "AIzaSyDpAJCXpexR5cBYeUikgfWpVyTkeIXF6qw",
+  "authDomain": "opa-player-database.firebaseapp.com",
+  "projectId": "opa-player-database",
+  "storageBucket": "opa-player-database.firebasestorage.app",
+  "messagingSenderId": "558478324565",
+  "appId": "1:558478324565:web:44537261c3da9fd6c775dc",
+  "measurementId": "G-RSJ2WQRWYJ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
