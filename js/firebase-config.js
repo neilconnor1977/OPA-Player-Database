89a5467a09a35dcec6a945c242ed4b032ff51a78
neilconnor1977 <neilconnor1977@gmail.com>
2025-08-27 18:46:03 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
-// Firebase configuration
-const firebaseConfig = {
-  "apiKey": "AIzaSyDpAJCXpexR5cBYeUikgfWpVyTkeIXF6qw",
-  "authDomain": "opa-player-database.firebaseapp.com",
-  "projectId": "opa-player-database",
-  "storageBucket": "opa-player-database.firebasestorage.app",
-  "messagingSenderId": "558478324565",
-  "appId": "1:558478324565:web:44537261c3da9fd6c775dc",
-  "measurementId": "G-RSJ2WQRWYJ"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Check if Firebase is already defined to avoid duplicate initialization
+if (typeof firebase === 'undefined') {
+  // Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDpAJCXpexR5cBYeUikgfWpVyTkeIXF6qw",
+    authDomain: "opa-player-database.firebaseapp.com",
+    projectId: "opa-player-database",
+    storageBucket: "opa-player-database.firebasestorage.app",
+    messagingSenderId: "558478324565",
+    appId: "1:558478324565:web:44537261c3da9fd6c775dc",
+    measurementId: "G-RSJ2WQRWYJ"
+  };
